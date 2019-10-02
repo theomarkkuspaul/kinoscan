c931956e2333a7fc825565e51d45af99d8c1007a
Theo Paul <theo.markkus.paul@gmail.com>
2019-10-03 08:37:23 +1300

    Remove dead code from main kinoscan file

--- lib/kinoscan.rb
+++ lib/kinoscan.rb
@@ -8,12 +8,3 @@ module Kinoscan
   class Error < StandardError; end
   # Your code goes here...
 end
-
-# files = Dir.entries(IMAGES_DIR).select {|f| !File.directory? f}
-# files.delete('.DS_Store')
-
-# files.each do |file_path|
-#   file_path = "photos/#{file_path}"
-#   scanner = KinoScan.new(file_path)
-#   scanner.call
-# end
